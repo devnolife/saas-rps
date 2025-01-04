79fbdba7200351617a21a3c085decd2b1b950a2c
devnolife <andi_agung@student.unismuh.ac.id>
2025-01-04 11:52:23 +0800

    refactor: add logging for document generation response in page component

--- src/app/(dashboard)/bahan-ajar/page.jsx
+++ src/app/(dashboard)/bahan-ajar/page.jsx
@@ -117,6 +117,8 @@ export default function Page() {
 
     try {
       const response = await axios.post('/api/bahan-ajar', result, { responseType: 'blob' });
+
+      console.log("🚀 ~ handleGenerateDocument ~ response:", response)
       const courseName = result?.matakuliah_info?.nama?.replace(/\s+/g, '_') || 'document';
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
